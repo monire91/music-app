f4b91eb1c23711c32e3a7fd9b9de3f9dfb28db14
monireh <aglida1370@gmail.com>
2020-01-11 22:58:03 +0330

    first commit

--- server.js
+++ server.js
@@ -1,32 +1,5 @@
-const cors =require('cors');
-const  connectDB = require('./db');
-connectDB();
-
 const express = require('express');
-// const fileUpload = require('express-fileupload')
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
-
-
-
-
-
-// const fileUpload = require('express-fileupload')
-// app.use(fileUpload())
-
-// app.use('./upload', express.static('upload'))
-app.use('/upload',express.static(__dirname+ '/upload'));
-
-// Routes
-// app.use('/api/',require('./routes/api/onlylogged'));
-// app.use('/api',require('./routes/api/songs'))
-//app.use('/api',require('./routes/api/images'))
-app.use('/api',require('./routes/api/uploadSongs'));
-
-app.use('/api',require('./routes/api/posts'));
-app.use('/api',require('./routes/api/auth'));
 
 app.get('/', (req,res)=>{
     res.send('home pageeee');
